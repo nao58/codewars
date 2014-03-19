d0c5de24ab4dc0855ae607d491935e1089f0218f
MORI Naohiko <naohiko.mori@gmail.com>
2014-03-19 18:29:15 +0900

    fix

--- lib/kata002.rb
+++ lib/kata002.rb
@@ -1,10 +1,5 @@
 class Kata002
   def self.fizzbuzz n
-    if n > 10
-      ans = []
-    else
-      ans = []
-    end
     ans = []
     1.upto(n) do |num|
       v = ""
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,4 @@
 require 'coveralls'
 Coveralls.wear!
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each{|file| require file}
